f9b15f4c7287c0325af0edb18fe80a9d8fcc248f
SanketParab3004 <200555449@student.georgianc.on.ca>
2024-12-11 16:30:28 -0500

    Remove all files from the repository

--- test.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch import Elasticsearch
-
-es = Elasticsearch(
-    ['https://localhost:9200'],
-    verify_certs=True,
-    ca_certs='path_to_http_ca.crt',
-    client_cert='path_to_client_cert.pem',
-    client_key='path_to_client_key.pem'
-)
